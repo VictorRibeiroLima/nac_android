d95e5f499d87b996256e84acfdae1f47e0fa75d9
Diego Ohanes Seraidarian <RM79740@fiap.com.br>
2019-10-17 10:36:38 -0300

    ola

---
+++ app/src/main/java/com/requisicoes/cursoandroid/requisicoeshttp/MainActivity.java
@@ -0,0 +1,169 @@
+package com.requisicoes.cursoandroid.requisicoeshttp;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botaoRecuperar;
+    private TextView resultadoCep;
+    private TextView resultadoLogradouro;
+    private TextView resultadoComplemento;
+    private TextView resultadoBairro;
+    private TextView resultadoLocalidade;
+    private TextView resultadoUf;
+    private TextView resultadoUnidade;
+    private TextView resultadoIbge;
+    private TextView resultadoGia;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoRecuperar = findViewById(R.id.buttonRecuperar);
+        resultadoCep = findViewById(R.id.resultadoCep);
+        resultadoLogradouro = findViewById(R.id.resultadoLogradouro);
+        resultadoComplemento = findViewById(R.id.resultadoComplemento);
+        resultadoBairro = findViewById(R.id.resultadoBairro);
+        resultadoLocalidade = findViewById(R.id.resultadoLocalidade);
+        resultadoUf = findViewById(R.id.resultadoUf);
+
+        botaoRecuperar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyTask task = new MyTask();
+                String urlApi = "https://blockchain.info/ticker";
+                String cep = "01310100";
+                String urlCep = "https://viacep.com.br/ws/" + cep + "/json/";
+                task.execute(urlApi);
+            }
+        });
+
+    }
+
+    class MyTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String stringUrl = strings[0];
+            InputStream inputStream = null;
+            InputStreamReader inputStreamReader = null;
+            StringBuffer buffer = null;
+
+            try {
+
+                URL url = new URL(stringUrl);
+                HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
+
+                // Recupera os dados em Bytes
+                inputStream = conexao.getInputStream();
+
+                //inputStreamReader lê os dados em Bytes e decodifica para caracteres
+                inputStreamReader = new InputStreamReader( inputStream );
+
+                //Objeto utilizado para leitura dos caracteres do InpuStreamReader
+                BufferedReader reader = new BufferedReader( inputStreamReader );
+                buffer = new StringBuffer();
+                String linha = "";
+
+                while((linha = reader.readLine()) != null){
+                    buffer.append( linha );
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return buffer.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String resultado) {
+            super.onPostExecute(resultado);
+
+            String logradouro = null;
+            String cep = null;
+            String complemento = null;
+            String bairro = null;
+            String localidade = null;
+            String uf = null;
+            /*
+            String objetoValor = null;
+            String valorMoeda = null;
+            String simbolo = null;
+
+            String objetoValorUS = null;
+            String valorMoedaUS = null;
+            String simboloUS = null;
+            */
+            try {
+
+                JSONObject jsonObject = new JSONObject(resultado);
+                logradouro = jsonObject.getString("logradouro");
+                cep = jsonObject.getString("cep");
+                complemento = jsonObject.getString("complemento");
+                bairro = jsonObject.getString("bairro");
+                localidade = jsonObject.getString("localidade");
+                uf = jsonObject.getString("uf");
+
+                /*valor para Real BRL
+                JSONObject jsonObject = new JSONObject(resultado);
+                objetoValor = jsonObject.getString("BRL");
+
+                //Valor para Dolar USD
+                JSONObject jsonObjectUS = new JSONObject(resultado);
+                objetoValorUS = jsonObjectUS.getString("USD");
+
+
+                //Valor para Real
+                JSONObject jsonObjectReal = new JSONObject(objetoValor);
+                valorMoeda = jsonObjectReal.getString("last");
+                simbolo = jsonObjectReal.getString("symbol");
+
+                //Valor para Dolar
+                JSONObject jsonObjectDolar = new JSONObject(objetoValorUS);
+                valorMoedaUS = jsonObjectDolar.getString("last");
+                simboloUS = jsonObjectDolar.getString("symbol");
+                */
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            //textoResultado.setText(resultado);
+            //textoResultado.setText(logradouro+" / "+cep+" / "+complemento+" / "+bairro+" / "+localidade+" / "+uf);
+            resultadoCep.setText("CEP: "+ cep);
+            resultadoLogradouro.setText("Logradouro: "+ logradouro);
+            resultadoComplemento.setText("Complemento: "+ complemento);
+            resultadoBairro.setText("Bairro: "+ bairro);
+            resultadoLocalidade.setText("Localidade: "+ localidade);
+            resultadoUf.setText("UF: "+ uf);
+        }
+    }
+
+}
